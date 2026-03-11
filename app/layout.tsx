4dbfe4ebe50b77e36afec644ea633fa4c0417012
grocerysushi <kylenotkyle1@gmail.com>
2026-03-11 18:45:57 -0500

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import LiturgicalTheme from "@/components/LiturgicalTheme";
 
@@ -15,6 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C75CMC27YN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C75CMC27YN');
+          `}
+        </Script>
+      </head>
       <body className="antialiased">
         <LiturgicalTheme>{children}</LiturgicalTheme>
       </body>
