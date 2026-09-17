1cad98f31a29d93169a0a5c257147860706da1bf
grocerysushi <kylenotkyle1@gmail.com>
2026-09-16 21:32:10 -0500

    Add AdSense site ownership verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,9 @@ export const metadata: Metadata = {
   authors: [{ name: siteConfig.name, url: siteConfig.url }],
   creator: siteConfig.name,
   publisher: siteConfig.name,
+  other: {
+    "google-adsense-account": "ca-pub-6122544703023536",
+  },
   alternates: {
     canonical: "/",
   },
